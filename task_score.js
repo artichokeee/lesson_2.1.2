e3c20d3ba68cc9f50e4198168a9eee26d6c6bf6c
artichokeee <rusau.qalife@gmail.com>
2023-10-14 16:18:50 +0300

    added task_score.js for Task 2.1.3

---
+++ task_score.js
@@ -0,0 +1,11 @@
+let introductionScore = 10;
+let gitScore = 10;
+let jsScore = 9;
+let avgScore = (introductionScore + gitScore + jsScore)/3;
+
+console.log(`Средний балл по трем модулям равен ${avgScore}`);
+
+let fname = "Иван";
+let lname = "Петров";
+
+console.log(`${fname} ${lname}: средний балл  по трем модулям равен ${avgScore}`);
